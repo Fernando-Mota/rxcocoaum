61a6e48490b8dd30fac7c1c234880b6641c19524
Fernando Mota e Silva <fm.aken@gmail.com>
2018-08-30 14:14:07 -0300

    Initial Commit

---
+++ Controls/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Controls
+//
+//  Created by Fernando Mota e Silva on 30/08/2018.
+//  Copyright © 2018 HalphDem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
